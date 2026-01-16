03b303c68f5e80a7f729a7319aa109eff8c6c4de
MAHAK AGARWAL <mahaka365@gmail.com>
2026-01-16 12:01:58 +0530

    Time: 5 ms (78.94%), Space: 77.3 MB (55.53%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int maxArea(int[] height) {
+
+        int left = 0;
+        int right = height.length - 1;
+        int maxArea = 0;
+
+        while (left < right) {
+            int currArea =
+                Math.min(height[left], height[right]) * (right - left);
+
+            maxArea = Math.max(maxArea, currArea);
+
+            // Move the pointer with smaller height
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return maxArea;
+    }
+}
